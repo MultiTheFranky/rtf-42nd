21e405403d07d09b41f45a06b53a2cd0fc492fba
Francisco Álamo García <fran.minecraft.mta@gmail.com>
2023-12-05 18:56:54 +0100

    fix: tab indentation

--- addons/music/CfgMusic.hpp
+++ addons/music/CfgMusic.hpp
@@ -1,10 +1,10 @@
 class CfgMusic
 {
-	tracks[] = {};
+    tracks[] = {};
 
-	class GVAR(MainMenu)
-	{
-		name	= "Main menu theme";
-		sound[]	= { QPATHTOF(data\rtf42_main_menu.ogg), "db + 1", 1.0 };
-	};
+    class GVAR(MainMenu)
+    {
+        name	= "Main menu theme";
+        sound[]	= { QPATHTOF(data\rtf42_main_menu.ogg), "db + 1", 1.0 };
+    };
 };

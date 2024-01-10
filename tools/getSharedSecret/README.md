662f920b7072b45b98c21e8a99c8932434e82957
Francisco Álamo García <fran.minecraft.mta@gmail.com>
2024-01-10 21:10:24 +0100

    feat: added version generator

---
+++ tools/getSharedSecret/README.md
@@ -0,0 +1,6 @@
+# Get shared secret
+This script was created to get the shared secret of an Steam account to allow protected accounts to make workflows.
+
+## How it works?
+
+Create a .env file, fill with your data and execute. The steam account needs to don't have steam guard enabled. These process will enabled it and output all the information to generate that steam guard key without the app.
---
+++ tools/version_generator/index.js
@@ -0,0 +1,95 @@
+/**
+ * This scripts will modify the version of the .hemtt/project.toml file and the addons/main/script_version.hpp file to a new version.
+ * It will also create a new commit and tag it with the new version.
+ * The format of the verion is: vyear.month.minor-rc.x
+ * The format of the version on the script_version.hpp file is:
+ * #define MAJOR year
+ * #define MINOR month
+ * #define PATCHLVL minor
+ * #define BUILD beta ? rc.x : 0
+ *
+ */
+
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
+
+function writeAddonsReleaseVersion(year, month, minor, beta) {
+    const releasePath = path.join(__dirname, '..', '..', 'addons', 'main', 'script_version.hpp');
+    const release = fs.readFileSync(releasePath, 'utf8');
+
+    const newRelease = release
+        .replace(/#define MAJOR (.*)/, `#define MAJOR ${year}`)
+        .replace(/#define MINOR (.*)/, `#define MINOR ${month}`)
+        .replace(/#define PATCHLVL (.*)/, `#define PATCHLVL ${minor}`)
+        .replace(/#define BUILD (.*)/, `#define BUILD ${beta ? beta : '0'}`);
+
+    fs.writeFileSync(releasePath, newRelease);
+}
+
+function writeProjectTomlVersion(year, month, minor, beta) {
+    const projectTomlPath = path.join(__dirname, '..', '..', '.hemtt', 'project.toml');
+    const projectToml = fs.readFileSync(projectTomlPath, 'utf8');
+
+    const newProjectToml = projectToml
+        .replace(/version = "(.*)"/, `version = "v${year}.${month}.${minor}${beta ? `-${beta}` : ''}"`);
+
+    fs.writeFileSync(projectTomlPath, newProjectToml);
+}
+
+function generateNewVersion(beta = false) {
+    const projectTomlPath = path.join(__dirname, '..', '..', '.hemtt', 'project.toml');
+    const projectToml = fs.readFileSync(projectTomlPath, 'utf8');
+
+    const version = projectToml.match(/version = "(.*)"/)[1];
+    const versionParts = version.split('.');
+    const versionYear = new Date().getFullYear();
+    const versionMonth = new Date().getMonth() + 1;
+    const versionMinor = versionParts[2].split('-')[0];
+
+    if (beta) {
+        if (!versionParts[3]) {
+            return {
+                version: `v${versionYear}.${versionMonth}.${parseInt(versionMinor) + 1}-rc.0`,
+                versionYear,
+                versionMonth,
+                versionMinor,
+                versionBetaMinor: 1,
+            };
+        }
+        const versionBetaMinor = versionParts[3];
+        return {
+            version: `v${versionYear}.${versionMonth}.${parseInt(versionMinor)}-rc.${parseInt(versionBetaMinor) + 1}`,
+            versionYear,
+            versionMonth,
+            versionMinor,
+            versionBetaMinor: parseInt(versionBetaMinor) + 1,
+        };
+    }
+
+    return {
+        version: `v${versionYear}.${versionMonth}.${parseInt(versionMinor) + 1}`,
+        versionYear,
+        versionMonth,
+        versionMinor,
+    };
+}
+
+// Get arguments
+const beta = process.argv.includes('--beta');
+
+// Generate new version
+const newVersion = generateNewVersion(beta);
+
+// Write new version
+writeAddonsReleaseVersion(newVersion.versionYear, newVersion.versionMonth, newVersion.versionMinor, beta ? newVersion.versionBetaMinor : null);
+writeProjectTomlVersion(newVersion.versionYear, newVersion.versionMonth, newVersion.versionMinor, beta ? `rc.${newVersion.versionBetaMinor}` : null);
+
+// Commit and tag
+const projectTomlPath = path.join(__dirname, '..', '..', '.hemtt', 'project.toml');
+const scriptVersionPath = path.join(__dirname, '..', '..', 'addons', 'main', 'script_version.hpp');
+execSync(`git add ${projectTomlPath} ${scriptVersionPath}`);
+execSync(`git commit -m "Version ${newVersion.version}"`);
+
+// Return version to be use on github actions
+console.log(newVersion.version);

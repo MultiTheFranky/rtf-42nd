5b9d11d8b0d64fb1efc7c14a07fc2901b2a916e0
Francisco Álamo García <fran.minecraft.mta@gmail.com>
2024-01-19 19:35:08 +0100

    feat: initial mission addon structure

---
+++ addons/missions/CfgMissions.hpp
@@ -0,0 +1,7 @@
+class CfgMissions
+{
+	class MPMissions
+	{
+		MPMISSION(GVAR(001),CSTRING(Mission1),QPATHTOF(data\mission1.saralite))
+	};
+};
---
+++ addons/missions/config.cpp
@@ -0,0 +1,17 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"rtf42_main"};
+        author = ECSTRING(main,Author);
+        authors[] = {ECSTRING(main,Author)};
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgEventHandlers.hpp"
+#include "CfgMissions.hpp"

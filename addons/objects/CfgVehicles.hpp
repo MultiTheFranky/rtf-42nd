db4b6e49cf30148892ec5b47cd74683e1876667d
Francisco Álamo García <fran.minecraft.mta@gmail.com>
2024-01-22 19:04:43 +0100

    fix: validation

--- addons/objects/CfgVehicles.hpp
+++ addons/objects/CfgVehicles.hpp
@@ -39,55 +39,55 @@ class CfgVehicles {
         author = ECSTRING(main,Author);
         vehicleClass = "Cargo";
         editorCategory = "EdCat_Things";
-		editorSubcategory = "EdSubcat_Lamps";
+        editorSubcategory = "EdSubcat_Lamps";
         model = QPATHTOF(data\ceiling_lamp.p3d);
         _generalMacro = QGVAR(CeilingLamp);
         class SimpleObject
-		{
-			eden = 0;
-			animate[] = {{"light_1_pitch_rot",0},{"light_1_jaw_rot",0.13}};
-			hide[] = {"light_1_hide"};
-			verticalOffset = 1.084;
-			verticalOffsetWorld = 0;
-			init = "''";
-		};
+        {
+            eden = 0;
+            animate[] = {{"light_1_pitch_rot",0},{"light_1_jaw_rot",0.13}};
+            hide[] = {"light_1_hide"};
+            verticalOffset = 1.084;
+            verticalOffsetWorld = 0;
+            init = "''";
+        };
         class AnimationSources {
-			class Light_1_pitch_source {
-				source = "user";
-				initPhase = 0;
-				animPeriod = "1/45";
-			};
-			class Light_1_jaw_source {
-				source = "user";
-				initPhase = 0.13;
-				animPeriod = 2;
-			};
-		};
+            class Light_1_pitch_source {
+                source = "user";
+                initPhase = 0;
+                animPeriod = "1/45";
+            };
+            class Light_1_jaw_source {
+                source = "user";
+                initPhase = 0.13;
+                animPeriod = 2;
+            };
+        };
         class Reflectors {
-			class Light_1 {
-				color[] = {1000,1000,1200};
-				ambient[] = {10,10,12};
-				intensity = 5;
-				size = 1;
-				innerAngle = 60;
-				outerAngle = 130;
-				coneFadeCoef = 6;
-				position = "Light_1_pos";
-				direction = "Light_1_dir";
-				hitpoint = "Light_1_hitpoint";
-				selection = "Light_1_hide";
-				useFlare = 1;
-				flareSize = 0.9;
-				flareMaxDistance = 250;
-				class Attenuation {
-					start = 0;
-					constant = 0;
-					linear = 2;
-					quadratic = 0.5;
-					hardLimitStart = 50;
-					hardLimitEnd = 70;
-				};
-			};
-		};
+            class Light_1 {
+                color[] = {1000,1000,1200};
+                ambient[] = {10,10,12};
+                intensity = 5;
+                size = 1;
+                innerAngle = 60;
+                outerAngle = 130;
+                coneFadeCoef = 6;
+                position = "Light_1_pos";
+                direction = "Light_1_dir";
+                hitpoint = "Light_1_hitpoint";
+                selection = "Light_1_hide";
+                useFlare = 1;
+                flareSize = 0.9;
+                flareMaxDistance = 250;
+                class Attenuation {
+                    start = 0;
+                    constant = 0;
+                    linear = 2;
+                    quadratic = 0.5;
+                    hardLimitStart = 50;
+                    hardLimitEnd = 70;
+                };
+            };
+        };
     };
 };

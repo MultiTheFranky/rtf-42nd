9cbbde12913561ad2aa1c0a990ecfecc3b8b1946
Francisco Álamo García <fran.minecraft.mta@gmail.com>
2024-02-10 00:29:10 +0100

    fix: validation

--- addons/animations/CfgMovesMaleSdr.hpp
+++ addons/animations/CfgMovesMaleSdr.hpp
@@ -1,30 +1,30 @@
 class CfgMovesMaleSdr {
-	class States {
-		class Acts_AidlPercMstpSloWWpstDnon_warmup_1_loop;
-		class AmovPercMstpSnonWnonDnon;
-		class GVAR(base): AmovPercMstpSnonWnonDnon {
-			enableDirectControl = 1;
-			showWeaponAim = 0;
-			relSpeedMin = 1;
-			relSpeedMax = 1;
-			disableWeaponsLong = 1;
-			disableWeapons = 1;
-			enableOptics = 0;
-			head = "headNo";
-			enableBinocular = 0;
-			headBobMode = 5;
-			headBobStrength = -1;
-			forceAim = 1;
-			ConnectTo[] = {};
-			InterpolateTo[] = {};
-			actions = "NoActions";
-		};
-        class GVAR(base_pistol): Acts_AidlPercMstpSloWWpstDnon_warmup_1_loop{};
-		class GVAR(fullpose_standby): GVAR(base) {
-			file = QPATHTOF(data\fullpose\rtf42_fullpose_standby.rtm);
-			speed = 1e-10;
-			ConnectTo[] = {"AmovPercMstpSnonWnonDnon",0.01};
-			InterpolateTo[] = {};
-		};
+    class States {
+        class Acts_AidlPercMstpSloWWpstDnon_warmup_1_loop;
+        class AmovPercMstpSnonWnonDnon;
+        class GVAR(base): AmovPercMstpSnonWnonDnon {
+            enableDirectControl = 1;
+            showWeaponAim = 0;
+            relSpeedMin = 1;
+            relSpeedMax = 1;
+            disableWeaponsLong = 1;
+            disableWeapons = 1;
+            enableOptics = 0;
+            head = "headNo";
+            enableBinocular = 0;
+            headBobMode = 5;
+            headBobStrength = -1;
+            forceAim = 1;
+            ConnectTo[] = {};
+            InterpolateTo[] = {};
+            actions = "NoActions";
+        };
+        class GVAR(base_pistol): Acts_AidlPercMstpSloWWpstDnon_warmup_1_loop {};
+        class GVAR(fullpose_standby): GVAR(base) {
+            file = QPATHTOF(data\fullpose\rtf42_fullpose_standby.rtm);
+            speed = 1e-10;
+            ConnectTo[] = {"AmovPercMstpSnonWnonDnon",0.01};
+            InterpolateTo[] = {};
+        };
     };
 };
--- addons/animations/CfgVehicles.hpp
+++ addons/animations/CfgVehicles.hpp
@@ -1,7 +1,7 @@
 class CfgVehicles {
-	class Man;
-	class CAManBase: Man {
-		class ACE_SelfActions {
+    class Man;
+    class CAManBase: Man {
+        class ACE_SelfActions {
             class GVAR(actions) {
                 displayName = "Actions"; //TODO: Translate
                 icon = ""; //TODO: Add icon
@@ -16,12 +16,12 @@ class CfgVehicles {
                     displayName = "Animation"; //TODO: Translate
                 };
                 class GVAR(actions_cancel) {
-					displayName = "Cancel"; //TODO: Translate
-					icon = ""; // TODO: Add icon
-					condition = "true";
-					exceptions[] = {};
-					statement = "ace_player playActionNow 'GestureNod';[ace_player, ''] remoteExec ['switchMove', 0];ace_player action ['SwitchWeapon', ace_player, ace_player, 0];";
-				};
+                    displayName = "Cancel"; //TODO: Translate
+                    icon = ""; // TODO: Add icon
+                    condition = "true";
+                    exceptions[] = {};
+                    statement = "ace_player playActionNow 'GestureNod';[ace_player, ''] remoteExec ['switchMove', 0];ace_player action ['SwitchWeapon', ace_player, ace_player, 0];";
+                };
             };
         };
     };

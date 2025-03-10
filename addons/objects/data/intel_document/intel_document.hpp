b4e4f4ee00860c58174351961bc1a92eca66c129
Francisco Alamo Garcia <fran.minecraft.mta@gmail.com>
2025-03-10 22:43:06 +0100

    feat: update intel document textures and add new variants

--- addons/objects/data/intel_document/intel_document.hpp
+++ addons/objects/data/intel_document/intel_document.hpp
@@ -5,7 +5,7 @@ class GVAR(intel_document_wrinkled): Leaflet_05_F {
     author = ECSTRING(main,Author);
     editorCategory = QEGVAR(main,Items);
     editorSubcategory = QEGVAR(main,Props);
-    hiddenSelectionsTextures[] = {QPATHTOF(data\intel_document\intel_document_co.paa)};
+    hiddenSelectionsTextures[] = {QPATHTOF(data\intel_document\intel_document_ca.paa)};
     editorPreview = QPATHTOF(data\intel_document\intel_document_wrinkled.jpg);
     #include "actions.hpp"
 };
@@ -18,7 +18,7 @@ class GVAR(intel_document): Leaflet_05_New_F {
     editorCategory = QEGVAR(main,Items);
     editorSubcategory = QEGVAR(main,Props);
     model = QPATHTOF(data\intel_document\intel_document.p3d);
-    hiddenSelectionsTextures[] = {QPATHTOF(data\intel_document\intel_document_co.paa)};
+    hiddenSelectionsTextures[] = {QPATHTOF(data\intel_document\intel_document_ca.paa)};
     editorPreview = QPATHTOF(data\intel_document\intel_document.jpg);
     #include "actions.hpp"
 };
@@ -30,7 +30,7 @@ class GVAR(multi_intel_document): GVAR(intel_document) {
     author = ECSTRING(main,Author);
     editorCategory = QEGVAR(main,Items);
     editorSubcategory = QEGVAR(main,Props);
-    hiddenSelectionsTextures[] = {QPATHTOF(data\intel_document\intel_document_multi_co.paa)};
+    hiddenSelectionsTextures[] = {QPATHTOF(data\intel_document\intel_document_multi_ca.paa)};
     editorPreview = QPATHTOF(data\intel_document\intel_document_multi.jpg);
     #include "actions.hpp"
 };
@@ -42,7 +42,19 @@ class GVAR(top_secret_intel_document): GVAR(intel_document) {
     author = ECSTRING(main,Author);
     editorCategory = QEGVAR(main,Items);
     editorSubcategory = QEGVAR(main,Props);
-    hiddenSelectionsTextures[] = {QPATHTOF(data\intel_document\intel_document_top_secret_co.paa)};
+    hiddenSelectionsTextures[] = {QPATHTOF(data\intel_document\intel_document_top_secret_ca.paa)};
     editorPreview = QPATHTOF(data\intel_document\intel_document_top_secret.jpg);
+    #include "actions.hpp"
+};
+
+class GVAR(missile_intel_document): GVAR(intel_document) {
+    scope = 2;
+    scopeCurator = 2;
+    displayName = CSTRING(missile_intel_documentDisplayName);
+    author = ECSTRING(main,Author);
+    editorCategory = QEGVAR(main,Items);
+    editorSubcategory = QEGVAR(main,Props);
+    hiddenSelectionsTextures[] = {QPATHTOF(data\intel_document\intel_document_missile_ca.paa)};
+    editorPreview = QPATHTOF(data\intel_document\intel_document_missile.jpg);
     #include "actions.hpp"
 };
